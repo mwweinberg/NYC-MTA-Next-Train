2cc1adf3fd2707acf75b2e19f959c480d21d25fa
Michael Weinberg <mwweinberg@users.noreply.github.com>
2017-10-01 13:25:48 -0400

    extracts trains going to specified stations

--- nycmta.py
+++ nycmta.py
@@ -2,6 +2,7 @@ from google.transit import gtfs_realtime_pb2
 import urllib
 from pprint import pprint
 from protobuf_to_dict import protobuf_to_dict
+from itertools import chain
 
 #this of stations:
 #http://mtaapi.herokuapp.com/stations
@@ -20,17 +21,13 @@ feed.ParseFromString(response.read())
 #turns the feed into a dictionary
 useful_dict = protobuf_to_dict(feed)
 
-#prints the dicionary just to see what it says
-#probably ok to cut this once you figure all of this stuff out
-with open('output7.txt', 'wt') as out:
-    pprint(useful_dict, stream=out)
 
 
 useful_list = []
 
-#walks through each train to see if it is an active train
+#walks through each train entry to see if it is an active train
 #will eventually want to change it to 'range(len(useful_dict))'
-for i in range (4):
+for i in range (40):
     #this seems to be necessary, I'm not sure why
     if useful_dict['entity'][i]['id']:
         #adds the arrival information to useful_list
@@ -40,4 +37,7 @@ for i in range (4):
         except KeyError:
             pass
 
-pprint(useful_list)
+#pulls the entries tied to specific stops
+small_list = [ i for i in chain.from_iterable(useful_list) if i['stop_id'] == 'D03N' ]
+
+pprint(small_list)
