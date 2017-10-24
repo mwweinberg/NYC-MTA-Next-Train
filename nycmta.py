7a99e9a347151e84c037a01c5c519a760e4e5721
Michael Weinberg <mwweinberg@users.noreply.github.com>
2017-10-24 16:54:07 -0400

    extracts arrival times in minutes and adds to list

--- nycmta.py
+++ nycmta.py
@@ -7,9 +7,9 @@ from protobuf_to_dict import protobuf_to_dict
 from itertools import chain
 
 #for comparing the arrival times
-import datetime
+#import datetime
 import time
-import dateutil.relativedelta
+#import dateutil.relativedelta
 
 #current works with python 2
 
@@ -18,6 +18,7 @@ import dateutil.relativedelta
 #york is F18S and F18N
 #high is A40S and A40N
 
+#used D03N to test in the past
 station_one = 'D03N'
 
 #creates the feed
@@ -32,7 +33,7 @@ feed.ParseFromString(response.read())
 #turns the feed into a dictionary
 useful_dict = protobuf_to_dict(feed)
 #just for troubleshooting
-print useful_dict
+#print useful_dict
 
 #this will print out everything that comes through the feed for troubleshooting/planning purposes into a file
 #with open('useful_dict.txt', 'wt') as out:
@@ -61,18 +62,17 @@ small_list = [ i for i in chain.from_iterable(useful_list) if i['stop_id'] == st
 
 pprint(small_list)
 
-#extracts the times from the small_list
+#extracts the times from the small_list and adds to arrival_times list
 for i in small_list:
     #this is the arrival time of each train
     the_time = i['arrival']['time']
+    #the_time - time.time gives you the seconds between arrival time and current time
+    arrival_time_in_minutes = (int(the_time) - int(time.time()))/60
+    #add arrival time to arrival_times list
+    arrival_times.append(arrival_time_in_minutes)
     #just for troubleshooting
-    print the_time
-    print "*******"
-    #convert epoch time to real time
-    #diff scheduled time from now
-    dt1 = datetime.datetime.fromtimestamp(the_time)
-    dt2 = int(time.time())
-    print dt2
-        #time_difference = dateutil.relativedelta.relativedelta (dt1, dt2)
-        #print time_difference
-    #add diff to arrival time list
+    print arrival_time_in_minutes
+    
+
+#just for debugging
+print arrival_times
