70990badf61277b25a0b6a97166d330d13fa4605
Michael Weinberg <mwweinberg@users.noreply.github.com>
2017-10-30 03:23:54 -0400

    pulls, but does not process, High st

--- nycmta.py
+++ nycmta.py
@@ -18,14 +18,20 @@ import time
 #york is F18S and F18N
 #high is A40S and A40N
 
+
+
+#*************************************
+#**********F train feed creation******
+#*************************************
+
 #used D03N to test in the past
 york_south = 'F18S'
 york_north = 'F18N'
 
-#creates the feed
+#creates the feed for the F train
 
 #URL has API key so keep it in a different file
-feed_url = open('url.txt')
+feed_url = open('url_F.txt')
 
 feed = gtfs_realtime_pb2.FeedMessage()
 response = urllib.urlopen(feed_url.read())
@@ -86,3 +92,46 @@ for i in small_list_york_north:
 #just for debugging
 print "York south: %s" % arrival_times_york_south
 print "York north: %s" % arrival_times_york_north
+
+#close out the feed file
+feed_url.close()
+
+#*************************************
+#**********A/C train feed creation******
+#*************************************
+
+#used D03N to test in the past
+high_south = 'A40S'
+high_north = 'A40N'
+
+#creates the feed for the F train
+
+#URL has API key so keep it in a different file
+feed_url = open('url_AC.txt')
+
+feed = gtfs_realtime_pb2.FeedMessage()
+response = urllib.urlopen(feed_url.read())
+feed.ParseFromString(response.read())
+
+#turns the feed into a dictionary
+useful_dict = protobuf_to_dict(feed)
+
+#this will print out everything that comes through the feed for troubleshooting/planning purposes into a file
+#with open('useful_dict_AC.txt', 'wt') as out:
+     #pprint(useful_dict, stream=out)
+
+#this is the list of sub elements in useful_dict. basically it makes useful_dict slightly more manageable
+useful_list = []
+
+#walks through each train entry to see if it is an active train
+for i in range (len(useful_dict['entity'])):
+    #this seems to be necessary, I'm not sure why
+    if useful_dict['entity'][i]['id']:
+        #adds the arrival information to useful_list
+        try:
+            useful_list.append(useful_dict['entity'][i]['trip_update']['stop_time_update'])
+        #if it is an entry that it not an active train, ignores
+        except KeyError:
+            pass
+
+pprint(useful_list)
