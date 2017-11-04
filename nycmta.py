5b8c8122f5a8a86a66a805ffb89f9f62bc5cf987
Michael Weinberg <mwweinberg@users.noreply.github.com>
2017-11-04 16:47:30 -0400

    rewrote scraper as a function
    Originally this was written as a series of scraper steps. This version turns the scraping into a function that is called four times.

--- nycmta.py
+++ nycmta.py
@@ -9,170 +9,67 @@ from itertools import chain
 #for comparing the arrival times
 #import datetime
 import time
-#import dateutil.relativedelta
 
-#current works with python 2
+#imports the url variables from config.py in the same folder
+from config import *
 
-#this of stations:
-#http://mtaapi.herokuapp.com/stations
-#york is F18S and F18N
-#high is A40S and A40N
-
-
-
-#*************************************
-#**********F train feed creation******
-#*************************************
-
-#used D03N to test in the past
-york_south = 'F18S'
-york_north = 'F18N'
-
-#creates the feed for the F train
-
-#URL has API key so keep it in a different file
-feed_url = open('url_F.txt')
-
-feed = gtfs_realtime_pb2.FeedMessage()
-response = urllib.urlopen(feed_url.read())
-feed.ParseFromString(response.read())
-
-#turns the feed into a dictionary
-useful_dict = protobuf_to_dict(feed)
-
-#this will print out everything that comes through the feed for troubleshooting/planning purposes into a file
-#with open('useful_dict.txt', 'wt') as out:
-     #pprint(useful_dict, stream=out)
-
-#this is the list of sub elements in useful_dict. basically it makes useful_dict slightly more manageable
-useful_list = []
-
-
-
-#walks through each train entry to see if it is an active train
-for i in range (len(useful_dict['entity'])):
-    #this seems to be necessary, I'm not sure why
-    if useful_dict['entity'][i]['id']:
-        #adds the arrival information to useful_list
-        try:
-            useful_list.append(useful_dict['entity'][i]['trip_update']['stop_time_update'])
-        #if it is an entry that it not an active train, ignores
-        except KeyError:
-            pass
-
-#******************************
-#**********YORK SOUTH SECTION**
-#******************************
-
-#pulls the entries tied to specific stops
-small_list_york_south = [ i for i in chain.from_iterable(useful_list) if i['stop_id'] == york_south ]
-
-#list of arrival times to be filled by extracting info from small_list
+#variables to hold the lists
 arrival_times_york_south = []
-#extracts the times from the small_list and adds to arrival_times list
-for i in small_list_york_south:
-    #this is the arrival time of each train
-    the_time = i['arrival']['time']
-    #the_time - time.time gives you the seconds between arrival time and current time
-    arrival_time_in_minutes = (int(the_time) - int(time.time()))/60
-    #add arrival time to arrival_times list
-    arrival_times_york_south.append(arrival_time_in_minutes)
-
-
-#******************************
-#**********YORK NORTH SECTION**
-#******************************
-small_list_york_north = [ i for i in chain.from_iterable(useful_list) if i['stop_id'] == york_north ]
 arrival_times_york_north = []
-for i in small_list_york_north:
-    the_time = i['arrival']['time']
-    arrival_time_in_minutes = (int(the_time) - int(time.time()))/60
-    arrival_times_york_north.append(arrival_time_in_minutes)
-
-#just for debugging
-print "York south: %s" % arrival_times_york_south
-print "York north: %s" % arrival_times_york_north
-
-#close out the feed file
-feed_url.close()
-
-#*************************************
-#**********A/C train feed creation******
-#*************************************
-
-#used D03N to test in the past
-high_south = 'A40S'
-high_north = 'A40N'
-
-#URL has API key so keep it in a different file
-feed_url = open('url_AC.txt')
-
-feed = gtfs_realtime_pb2.FeedMessage()
-response = urllib.urlopen(feed_url.read())
-feed.ParseFromString(response.read())
-
-#turns the feed into a dictionary
-useful_dict = protobuf_to_dict(feed)
-
-#this will print out everything that comes through the feed for troubleshooting/planning purposes into a file
-#with open('useful_dict_AC.txt', 'wt') as out:
-     #pprint(useful_dict, stream=out)
-
-#this is the list of sub elements in useful_dict. basically it makes useful_dict slightly more manageable
-useful_list = []
-
-#walks through each train entry to see if it is an active train
-for i in range (len(useful_dict['entity'])):
-    #this seems to be necessary, I'm not sure why
-    if useful_dict['entity'][i]['id']:
-        #adds the arrival information to useful_list
-        try:
-            useful_list.append(useful_dict['entity'][i]['trip_update']['stop_time_update'])
-        #if it is an entry that it not an active train, ignores
-        except KeyError:
-            pass
-
-#for debugging
-#pprint(useful_list)
-
-#******************************
-#**********HIGH SOUTH SECTION**
-#******************************
-
-#pulls the entries tied to specific stops
-small_list_high_south = [ i for i in chain.from_iterable(useful_list) if i['stop_id'] == high_south ]
-
-#list of arrival times to be filled by extracting info from small_list
 arrival_times_high_south = []
-#extracts the times from the small_list and adds to arrival_times list
-for i in small_list_high_south:
-    #this is the arrival time of each train
-    the_time = i['arrival']['time']
-    #the_time - time.time gives you the seconds between arrival time and current time
-    arrival_time_in_minutes = (int(the_time) - int(time.time()))/60
-    #add arrival time to arrival_times list
-    arrival_times_high_south.append(arrival_time_in_minutes)
-
-#for debugging
-print "High south: %s" % arrival_times_high_south
-
-#******************************
-#**********HIGH NORTH SECTION**
-#******************************
-
-#pulls the entries tied to specific stops
-small_list_high_north = [ i for i in chain.from_iterable(useful_list) if i['stop_id'] == high_north ]
-
-#list of arrival times to be filled by extracting info from small_list
 arrival_times_high_north = []
-#extracts the times from the small_list and adds to arrival_times list
-for i in small_list_high_north:
-    #this is the arrival time of each train
-    the_time = i['arrival']['time']
-    #the_time - time.time gives you the seconds between arrival time and current time
-    arrival_time_in_minutes = (int(the_time) - int(time.time()))/60
-    #add arrival time to arrival_times list
-    arrival_times_high_north.append(arrival_time_in_minutes)
 
-#for debugging
-print "High north: %s" % arrival_times_high_north
+
+
+def grabber(station_ID, station_URL):
+
+    #grabs the feed
+    feed = gtfs_realtime_pb2.FeedMessage()
+    response = urllib.urlopen(station_URL)
+    feed.ParseFromString(response.read())
+
+    #turns the feed into a dictionary
+    useful_dict = protobuf_to_dict(feed)
+
+    #this is the list of sub elements in useful_dict. basically it makes useful_dict slightly more manageable
+    useful_list = []
+
+    #walks through each train entry to see if it is an active train
+    for i in range (len(useful_dict['entity'])):
+        #this seems to be necessary, I'm not sure why
+        if useful_dict['entity'][i]['id']:
+            #adds the arrival information to useful_list
+            try:
+                useful_list.append(useful_dict['entity'][i]['trip_update']['stop_time_update'])
+            #if it is an entry that it not an active train, ignores
+            except KeyError:
+                pass
+
+    #pulls the entries tied to specific stops
+    small_list_station = [ i for i in chain.from_iterable(useful_list) if i['stop_id'] == station_ID ]
+
+    #list of arrival times to be filled by extracting info from small_list
+    arrival_times_station = []
+    #extracts the times from the small_list and adds to arrival_times list
+    for i in small_list_station:
+        #this is the arrival time of each train
+        the_time = i['arrival']['time']
+        #the_time - time.time gives you the seconds between arrival time and current time
+        arrival_time_in_minutes = (int(the_time) - int(time.time()))/60
+        #add arrival time to arrival_times list
+        arrival_times_station.append(arrival_time_in_minutes)
+
+
+    return arrival_times_station
+
+
+arrival_times_york_south = grabber('F18S', URL_F)
+arrival_times_york_north = grabber('F18N', URL_F)
+arrival_times_high_south = grabber('A40S', URL_AC)
+arrival_times_high_north = grabber('A40N', URL_AC)
+
+
+print arrival_times_york_south
+print arrival_times_york_north
+print arrival_times_high_south
+print arrival_times_high_north
