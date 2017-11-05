46d842ab3dd436de0c8ddadcd9e43a7af2c5eb27
Michael Weinberg <mwweinberg@users.noreply.github.com>
2017-11-05 18:31:55 -0500

    oops

--- config.py
+++
@@ -1,3 +0,0 @@
-URL_F = 'http://datamine.mta.info/mta_esi.php?key=8ac9158789b34c0a9d88ed4295591555&feed_id=21'
-
-URL_AC = "http://datamine.mta.info/mta_esi.php?key=8ac9158789b34c0a9d88ed4295591555&feed_id=26"
