081522c158f6a43d0b6546699bda8abed540acfe
Michael Weinberg <mwweinberg@users.noreply.github.com>
2017-11-05 15:43:25 -0500

    creates list to send to arduino

--- nycmta.py
+++ nycmta.py
@@ -19,8 +19,11 @@ arrival_times_york_north = []
 arrival_times_high_south = []
 arrival_times_high_north = []
 
+#variable to hold the string to send to arduino
+light_list = []
 
 
+#function to scrape the MTA site and add the arrival times to arrival_times lists
 def grabber(station_ID, station_URL):
 
     #grabs the feed
@@ -62,14 +65,44 @@ def grabber(station_ID, station_URL):
 
     return arrival_times_station
 
+#functionto convert arrival_times lists to a string to send to arduino
+def lighter(arrival_list, light_one, light_two, light_three, light_four):
+    #walk through the numbers in the list
+    for item in arrival_list:
+        #convert the number to a number and see if it is in this
+        if 1 <= int(item) <= 8:
+            #if it is, add it to the list to send to the arduino
+            light_list.append(light_one)
+        elif 8 < int(item) <= 12:
+            light_list.append(light_two)
+        elif 12 < int(item) <=17:
+            light_list.append(light_three)
+        elif 17 < int(item) <=25:
+            light_list.append(light_four)
+        else:
+            pass
+
+    #clearn out arrival_times list for the next time around
+    #only relevant when this becomes a loop
+    arrival_list = []
+
+
 
 arrival_times_york_south = grabber('F18S', URL_F)
 arrival_times_york_north = grabber('F18N', URL_F)
 arrival_times_high_south = grabber('A40S', URL_AC)
 arrival_times_high_north = grabber('A40N', URL_AC)
 
-
+#debugging
 print arrival_times_york_south
 print arrival_times_york_north
 print arrival_times_high_south
 print arrival_times_high_north
+
+lighter(arrival_times_york_south, 'a', 'b', 'c', 'd')
+lighter(arrival_times_high_north, 'e', 'f', 'g', 'h')
+lighter(arrival_times_high_south, 'i', 'j', 'k', 'l')
+lighter(arrival_times_high_north, 'm', 'n', 'o', 'p')
+
+#debugging
+print light_list
